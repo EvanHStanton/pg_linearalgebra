04fefe37f2a6abeac00647789424c3a3f289bd03
Evan Stanton <evan.h.stanton@gmail.com>
2023-10-27 12:20:39 +0200

    update build.rs

--- build.rs
+++ build.rs
@@ -1,5 +1,6 @@
 fn main() {
     println!("cargo:rustc-link-search=native=/opt/homebrew/Cellar/openblas/lib");
     println!("cargo:rustc-link-lib=dylib=openblas");
+    println!("cargo:rustc-link-lib=dylib=lapack");
 }
 
